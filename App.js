98c045b8b78f33169dfd3d0b3f4d392094624b5c
Suhas <suhassaibadam@gmail.com>
2024-04-13 20:22:10 +0530

    EP-04 Part-01 : Created Header(logo-container, nav-items), Body(search-bar, restaurants-container) and RestaurantCard(res-logo, info)

--- App.js
+++ App.js
@@ -1,66 +1,92 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-// =============
 
-/*
-const Title = (
-  <h1 className="head" tabIndex="5">
-    Namaste React using a React Element
-  </h1>
-);
+/**
+ * "Header" Component
+ *  -> Logo
+ *  -> Nav Items
+ *    -> Home, AboutUs, Cart
+ * 
+ * "Body" Component
+ *  -> Search Bar
+ *  -> RestaurantContainer
+ *    -> "RestaurantCard" Component
+ * 
+ * "RestaurantCard" Component
+ *  -> FoodImage
+ *  -> Info
+ *    ->Name of the Reataurant, Cuisine, Ratings, Stars, Delivery Time etc..
+ * 
+ * "Footer" Component
+ *  -> CopyRights
+ *  -> Links
+ *  -> Contact
+ *  -> Address
+ **/
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Offers</li>
+          <li>Sign In</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+};
+
+const styleCard = {
+  backgroundColor : "#f0f0f0"
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="restaurant-card" style={styleCard}>
+      <img className="res-logo" alt="food-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xqwpuhgnsaf18te7zvtv"/>
+      <h3>Meghana Food</h3>
+      <h4>Biryani, South Indian, Chinese</h4>
+      <h4>4.4 Stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  )
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-bar">Search</div>
+      <div className="restaurants-container">
+        <RestaurantCard/> <RestaurantCard/>
+        <RestaurantCard/> <RestaurantCard/>
+        <RestaurantCard/> <RestaurantCard/>
+        <RestaurantCard/> <RestaurantCard/>
+        <RestaurantCard/> <RestaurantCard/>
+        <RestaurantCard/> <RestaurantCard/>
+        <RestaurantCard/> <RestaurantCard/>
+        <RestaurantCard/> <RestaurantCard/>
+      </div>
+    </div>
+  )
+};
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header/>
+      <Body/>
+    </div>
+  )
+};
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(Title);
-*/
-// ========================================================================
-
-
-// React Functional Component
-// ==========================
-
-/*
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    This is Namaste React Title
-  </h1>
-);
-
-
-const HeadingComponent = () => (
-  <div id="container">
-    <Title/>
-    <h1 className="heading">
-      Namaste React using Functional Component
-    </h1>
-  </div>
-);
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<HeadingComponent/>);
-*/
-
-// Putting a Component inside another Component is called "Component Composition".
-// =================================================================================
-
-
-
-
-
-
-// Inside JSX, we can write any JavaScript code enclosed in curly braces {}.
-
-const name = 'Suhas Sai Badam';
-const id = 'GSH@12345';
-
-const MainComponent = () => (
-  <div id="container">
-    <h1>{console.log('This is Namaste React Course')}</h1>
-    <h2>This is my Info:</h2>
-    <h3>{name}, {id}</h3>
-    {925/25}
-  </div>
-);
-
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<MainComponent/>);
+root.render(<AppLayout/>);
