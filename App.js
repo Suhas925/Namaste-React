bcb22d8f25eb36227cc11830532c557d625a8016
Suhas <suhassaibadam@gmail.com>
2024-04-14 12:24:38 +0530

    EP-04 Part-02(b) : Made 'RestaurantCard' component dynamic by passing swiggy data as props.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import resObj from "./Data.js";
 
 
 /**
@@ -49,92 +50,30 @@ const styleCard = {
 };
 
 
-// Now we are gonna make the RestaurantCard dynamic using "props"
-// props ---> properties.
-// props are something which we can pass it to the component.
-// Here I want to dynamically pass data to the RestaurantCard component, I can pass the data as a "prop".
-
-// Functional Component  ----> Normal JavaScript Function.
-// Props -----> Arguments to a Normal JavaScript Fucntion.
-
-// Function(arguments) -----> Component(props)
-// passing props to a component is similar to passing arguments to a function.
-
-// React basicaly takes all the properties (props) and it will wrap them inside an object, then it will pass them to the component as props.
-// Here 'props' is an object.
-
-
-
-
-/* Style-1 of using props
-=========================
 
 const RestaurantCard = (props) => {
+  const { resData } = props;
   return (
     <div className="restaurant-card" style={styleCard}>
-      <img className="res-logo" alt="food-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xqwpuhgnsaf18te7zvtv"/>
-      <h3>{props.resName}</h3>
-      <h4>{props.cuisine}</h4>
-      <h4>{props.stars}</h4>
-      <h4>{props.eta}</h4>
-    </div>
-  )
-};
-*/
-
-
-// Style-2 of using props
-// ======================
-
-const RestaurantCard = (props) => {
-  const {resName, cuisine, stars, eta} = props;
-  return (
-    <div className="restaurant-card" style={styleCard}>
-      <img className="res-logo" alt="food-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xqwpuhgnsaf18te7zvtv"/>
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>{stars}</h4>
-      <h4>{eta}</h4>
+      <img className="res-logo" alt="food-img"
+        src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" + resData.info.cloudinaryImageId}/>
+      <h3>{resData.info.name}</h3>
+      <h4>{resData.info.cuisines.join(", ")}</h4>
+      <h4>{resData.info.avgRating} Stars</h4>
+      <h4>{resData.info.costForTwo}</h4>
+      <h4>{resData.info.sla.deliveryTime} minutes</h4>
     </div>
   )
 };
 
 
-
-/* Style-3 of using props
-=========================
-Destructuring the object 'props' on the fly.
--------------------------------------------
-
-const RestaurantCard = ({resName, cuisine, stars, eta}) => {
-  return (
-    <div className="restaurant-card" style={styleCard}>
-      <img className="res-logo" alt="food-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xqwpuhgnsaf18te7zvtv"/>
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>{stars}</h4>
-      <h4>{eta}</h4>
-    </div>
-  )
-};
-*/
-
-
+// Below the 'resObj' is imported from 'Data.js' file.
 const Body = () => {
   return (
     <div className="body">
       <div className="search-bar">Search</div>
       <div className="restaurants-container">
-        <RestaurantCard
-          resName="Meghana Foods"
-          cuisine="Biryani, South Indian, Chinese"
-          stars="4.4 Stars"
-          eta="38 minutes"/>
-        <RestaurantCard
-          resName="KFC"
-          cuisine="Fried Chicken, Burgers, Wings"
-          stars="4.5 Stars"
-          eta="27 minutes"/>
+        <RestaurantCard resData={ resObj }/>
       </div>
     </div>
   )
---
+++ Data.js
@@ -0,0 +1,93 @@
+const resObj = {
+    "info": {
+      "id": "38634",
+      "name": "Third Wave Coffee",
+      "cloudinaryImageId": "d96267738c19ec62acb5390e52faba41",
+      "locality": "Koramangala",
+      "areaName": "Koramangala",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "Beverages",
+        "Bakery",
+        "Continental"
+      ],
+      "avgRating": 4.5,
+      "parentId": "274773",
+      "avgRatingString": "4.5",
+      "totalRatingsString": "5K+",
+      "sla": {
+        "deliveryTime": 25,
+        "lastMileTravel": 0.9,
+        "serviceability": "SERVICEABLE",
+        "slaString": "25-30 mins",
+        "lastMileTravelString": "0.9 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-04-15 03:00:00",
+        "opened": true
+      },
+      "badges": {
+        "imageBadges": [
+          {
+            "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+            "description": "Delivery!"
+          }
+        ]
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "Delivery!",
+                  "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png"
+                }
+              }
+            ]
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "20% OFF",
+        "subHeader": "UPTO ₹50"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/restaurants/third-wave-coffee-koramangala-bangalore-38634",
+      "type": "WEBLINK"
+    }
+  };
+
+export default resObj;
