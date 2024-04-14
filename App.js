870a9c54840cae6005b6f2017c52e3a6ad4b60f0
Suhas <suhassaibadam@gmail.com>
2024-04-14 14:07:45 +0530

    EP-04 Part-02(d) : Cleaning & Optmizing our code using Optional Chainaing, .map() function and using unique id as key.

--- App.js
+++ App.js
@@ -53,15 +53,17 @@ const styleCard = {
 
 const RestaurantCard = (props) => {
   const { resData } = props;
+  const {cloudinaryImageId, name, cuisines, avgRating, costForTwo} = resData?.info;
+  const {deliveryTime} = resData?.info?.sla;
   return (
     <div className="restaurant-card" style={styleCard}>
       <img className="res-logo" alt="food-img"
-        src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" + resData.info.cloudinaryImageId}/>
-      <h3>{resData.info.name}</h3>
-      <h4>{resData.info.cuisines.join(", ")}</h4>
-      <h4>{resData.info.avgRating} Stars</h4>
-      <h4>{resData.info.costForTwo}</h4>
-      <h4>{resData.info.sla.deliveryTime} minutes</h4>
+        src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" + cloudinaryImageId}/>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} Stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{deliveryTime} minutes</h4>
     </div>
   )
 };
@@ -73,18 +75,22 @@ const Body = () => {
     <div className="body">
       <div className="search-bar">Search</div>
       <div className="restaurants-container">
-        <RestaurantCard resData={ resList[0]}/>
-        <RestaurantCard resData={ resList[1]}/>
-        <RestaurantCard resData={ resList[2]}/>
-        <RestaurantCard resData={ resList[3]}/>
-        <RestaurantCard resData={ resList[4]}/>
-        <RestaurantCard resData={ resList[5]}/>
-        <RestaurantCard resData={ resList[6]}/>
-        <RestaurantCard resData={ resList[7]}/>
-        <RestaurantCard resData={ resList[8]}/>
-        <RestaurantCard resData={ resList[9]}/>
-        <RestaurantCard resData={ resList[10]}/>
-        <RestaurantCard resData={ resList[11]}/>
+        {
+          resList.map(restaurant => (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+          ))
+
+          /* we can also use 'index' as 'key'
+          resList.map((restaurant, index) => (
+            <RestaurantCard key={index} resData={restaurant}/>
+          ))
+          But React itself says that never use 'index' as the 'key'.
+          Its okay to use but it is not recommended.
+          Whenever you don't have a unique id, then only use 'index' as 'key'.
+          Not using keys is not acceptable.
+          $ Unique Id >>>>> index as key >> not using key
+          */
+        }
       </div>
     </div>
   )
