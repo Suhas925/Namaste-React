b90fd5093fda99d4377f97de7511d36375ee0ea6
Suhas <suhassaibadam@gmail.com>
2024-04-15 16:31:47 +0530

    EP-05 Part-02(c) : useState() hook with mockData. Brief about "Reconciliation Algorithm" or "React Fiber".

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,55 +7,7 @@ const Body = () => {
 
   // React Hook -> useState()
   // State Variable - Super Powerful Variable
-  const [listOfRestaurants, setListOfRestaurants] = useState(
-    [
-      {
-        "info": {
-          "id": "38634",
-          "name": "Third Wave Coffee",
-          "cloudinaryImageId": "d96267738c19ec62acb5390e52faba41",
-          "costForTwo": "₹400 for two",
-          "cuisines": [
-            "Beverages",
-            "Bakery",
-            "Continental"
-          ],
-          "avgRating": 4.5,
-          "sla": {"deliveryTime": 25}
-        }
-      },
-      {
-        "info": {
-          "id": "381853",
-          "name": "ITC Master Chef Creations",
-          "cloudinaryImageId": "c8439641cf359075cbbe91f6b3a1bb6e",
-          "costForTwo": "₹650 for two",
-          "cuisines": [
-            "North Indian",
-            "Biryani",
-            "Kebabs"
-          ],
-          "avgRating": 3.9,
-          "sla": {"deliveryTime": 39}
-        }
-      },
-      {
-        "info": {
-          "id": "107443",
-          "name": "Lo! - Low Carb and Keto Foods",
-          "cloudinaryImageId": "o8t0w5gdlujpigauaflw",
-          "costForTwo": "₹150 for two",
-          "cuisines": [
-            "Healthy Food",
-              "Keto",
-              "Continental"
-          ],
-          "avgRating": 4.2,
-          "sla": {"deliveryTime": 28}
-        }
-      }
-    ]
-  );
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
   // Normal JavaScript Variable
   /*
@@ -109,13 +61,12 @@ const Body = () => {
 
   return (
     <div className="body">
-      <div className="search-bar">Search</div>
       <div className="filter">
         <button className="filterBtn"
           onClick={() => {
             // Filter Logic Here
             const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4.0
+              (res) => res.info.avgRating > 4.3
             );
             setListOfRestaurants(filteredList);
           }}>
