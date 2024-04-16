81cff91bea055f548f3860d66c31de37e74f7413
Suhas <suhassaibadam@gmail.com>
2024-04-16 19:17:15 +0530

    EP-06 Part-03(a) : fetched the live data from swiggy.com by making an api call using fetch(). Populated the 'listOfRestaurants' with the new data.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,26 @@
-import resList from "../utils/dataObj.js";
 import RestaurantCard from "./RestaurantCard.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const Body = () => {
 
-  // React Hook -> useState()
   // State Variable - Super Powerful Variable
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
+  useEffect(()=>{
+    fetchData();
+  },
+  []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurants(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+  };
 
   return (
     <div className="body">
