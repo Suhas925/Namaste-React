856216cba6f582be76fb0133e64d78034b1a3d2f
Suhas <suhassaibadam@gmail.com>
2024-04-16 16:41:40 +0530

    EP-06 Part-02 : Approaches of fetching data from api/backend.

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,55 +9,6 @@ const Body = () => {
   // State Variable - Super Powerful Variable
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
-  // Normal JavaScript Variable
-  /*
-  let listOfRestaurants = [
-    {
-      "info": {
-        "id": "38634",
-        "name": "Third Wave Coffee",
-        "cloudinaryImageId": "d96267738c19ec62acb5390e52faba41",
-        "costForTwo": "₹400 for two",
-        "cuisines": [
-          "Beverages",
-          "Bakery",
-          "Continental"
-        ],
-        "avgRating": 4.5,
-        "sla": {"deliveryTime": 25}
-      }
-    },
-    {
-      "info": {
-        "id": "381853",
-        "name": "ITC Master Chef Creations",
-        "cloudinaryImageId": "c8439641cf359075cbbe91f6b3a1bb6e",
-        "costForTwo": "₹650 for two",
-        "cuisines": [
-          "North Indian",
-          "Biryani",
-          "Kebabs"
-        ],
-        "avgRating": 3.9,
-        "sla": {"deliveryTime": 39}
-      }
-    },
-    {
-      "info": {
-        "id": "107443",
-        "name": "Lo! - Low Carb and Keto Foods",
-        "cloudinaryImageId": "o8t0w5gdlujpigauaflw",
-        "costForTwo": "₹150 for two",
-        "cuisines": [
-          "Healthy Food",
-            "Keto",
-            "Continental"
-        ],
-        "avgRating": 4.2,
-        "sla": {"deliveryTime": 28}
-      }
-    }];
-  */
 
   return (
     <div className="body">
