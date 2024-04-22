b68af9c2085f9cb4dcdfac8bc76af4b9350d299a
Suhas <suhassaibadam@gmail.com>
2024-04-22 19:53:28 +0530

    EP-07 Part-02 : Do's and Don'ts of 'useState()' hook.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,15 @@ const Header = () => {
   console.log("Header Rendered");
 
   const [sign, setSign] = useState('Sign In');
+  // Never create the local state variables and never use 'useState()' hook outside of the component.
+  // useState() is used to create local state variables inside the functional components.
+  // Don't use useState() hook inside the 'if' block as well as 'for' loop.
+  // Don't create the state variables inside a function.
+
+  // Always use the 'useState()' hook inside a component.
+  // Best practice is to use it at the top level inside the component.
+
+  
 
   // If there is no dependency array, useEffect will be called everytime the component is rendered.
   // If the dependency array is empty [], useEffect will be called at the initial render only.
