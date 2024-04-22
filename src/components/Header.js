2e60e9b3f34b9a9c1c95873f58e14d3e97aa1ed9
Suhas <suhassaibadam@gmail.com>
2024-04-22 19:29:19 +0530

    EP-07 Part-01 : Working principle of 'useEffect()' hook. Few style changes of search input element.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import {LOGO_URL} from "../utils/constants.js";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 const Header = () => {
+  console.log("Header Rendered");
 
   const [sign, setSign] = useState('Sign In');
+
+  // If there is no dependency array, useEffect will be called everytime the component is rendered.
+  // If the dependency array is empty [], useEffect will be called at the initial render only.
+  useEffect(() => {
+  console.log("useEffect called");
+  },
+  [sign]);
+  // When we put something inside the dependency array, useEffect will be called only when the dependency changes.
+  // If the dependency array is [sign], then useEffect is called everytime the 'sign' is updated.
 
   const handleButton = () => {
     sign === 'Sign In'
